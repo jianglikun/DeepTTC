a9dd1a224a433bf2933a2d2728deb5611c95b52c
Oleksandr Narykov <onarykov@anl.gov>
2022-10-17 17:44:30 -0500

    Modifed train.sh interface

--- train.sh
+++ train.sh
@@ -5,25 +5,31 @@
 # arg 3 CANDLE_CONFIG
 
 CANDLE_MODEL=/DeepTTC/DeepTTC_candle.py
-if [[ "$#" -ne 2 ]] ; then
+if [[ "$#" -ne 2 && "$#" -ne 3 ]] ; then
 	    echo "Illegal number of parameters"
 	    echo "CUDA_VISIBLE_DEVICES CANDLE_DATA_DIR are required"
 		echo "CANDLE_CONFIG is an optional parameter. It defines a path to the parameter list for the model."
 	    exit -1
 fi
 
-if [ ! -z $CANDLE_CONFIG ]
-        if [ ! -f $CANDLE_CONFIG ]
-            echo "Cannot read configuration file! If you want to run model with default parameters leave third option empty."
-			exit -1
-        fi
-fi
-
 CUDA_VISIBLE_DEVICES=$1
 CANDLE_DATA_DIR=$2
 CANDLE_CONFIG=$3
 
-CMD="python ${CANDLE_MODEL} --config_file ${CANDLE_CONFIG}"
+CMD=""
+
+if [ ! -z "${CANDLE_CONFIG}" ]; then
+        if [ ! -f "$CANDLE_CONFIG" ]; then
+            echo "Cannot read configuration file! If you want to run model with default parameters leave third option empty."
+			exit -1
+		else
+			CMD="python ${CANDLE_MODEL} --config_file ${CANDLE_CONFIG}"
+        fi
+else
+	CMD="python ${CANDLE_MODEL}"
+fi
+
+
 
 echo "using container "
 echo "using CUDA_VISIBLE_DEVICES ${CUDA_VISIBLE_DEVICES}"
