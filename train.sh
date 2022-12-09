02ee60a9e850c850c020541bc8b989875e09613a
brettin <brettin@.anl.gov>
2022-12-09 15:36:55 +0000

    interface updates

--- train.sh
+++ train.sh
@@ -1,21 +1,42 @@
 #!/bin/bash
+set -x
 
 # arg 1 CUDA_VISIBLE_DEVICES
 # arg 2 CANDLE_DATA_DIR
 # arg 3 CANDLE_CONFIG
 
-CANDLE_MODEL=/DeepTTC/DeepTTC_candle.py
-if [[ "$#" < 2  ]] ; then
-	    echo "Illegal number of parameters"
-	    echo "CUDA_VISIBLE_DEVICES CANDLE_DATA_DIR are required"
-	    exit -1
+CANDLE_MODEL="/DeepTTC/DeepTTC_candle.py"
+
+if [ $# -lt 2 ] ; then
+        echo "Illegalnumber of paramaters"
+        echo "Illegal number of parameters"
+        echo "CUDA_VISIBLE_DEVICES CANDLE_DATA_DIR are required"
+        exit -1
 fi
 
-CUDA_VISIBLE_DEVICES=$1; shift
-CANDLE_DATA_DIR=$1; shift
-CMD="python3 ${CANDLE_MODEL} $@"
+if [ $# -eq 2 ] ; then
+        CUDA_VISIBLE_DEVICES=$1 ; shift
+        CANDLE_DATA_DIR=$1 ; shift
+        CMD="python ${CANDLE_MODEL}"
+        echo "CMD = $CMD"
 
+elif [ $# -ge 3 ] ; then
+        CUDA_VISIBLE_DEVICES=$1 ; shift
+        CANDLE_DATA_DIR=$1 ; shift
 
+        # if $3 is a file, then set candle_config
+        if [ -f $CANDLE_DATA_DIR/$1 ] ; then
+                CANDLE_CONFIG=$1 ; shift
+                CMD="python ${CANDLE_MODEL} --config_file $CANDLE_CONFIG $@"
+                echo "CMD = $CMD $@"
+
+        # else passthrough $@
+        else
+                CMD="python ${CANDLE_MODEL} $@"
+                echo "CMD = $CMD"
+
+        fi
+fi
 
 echo "using container "
 echo "using CUDA_VISIBLE_DEVICES ${CUDA_VISIBLE_DEVICES}"
