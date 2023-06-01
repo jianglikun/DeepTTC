8375427e7a769acf85192b59e0a97769b1ffb9e0
Andreas Wilke <andreas.wilke@me.com>
2023-06-01 11:57:59 -0500

    Update train.sh
    Changed CANDLE_MODEL="DeepTTC_candle.py"

--- train.sh
+++ train.sh
@@ -11,7 +11,7 @@ set -x
 # arg 3 CANDLE_CONFIG
 
 ### Path to your CANDLEized model's main Python script###
-CANDLE_MODEL="DeepTTC_candle.py"
+CANDLE_MODEL="deepttc_baseline_pytorch.py"
 
 # Path to directory containing model executable
 IMPROVE_MODEL_DIR=${IMPROVE_MODEL_DIR:-$( dirname -- "$0" )}
